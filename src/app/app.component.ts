0bcbea41542b3cb5440da321460d028a5f8090e8
MorganMBA <mba@intechinfo.fr>
2019-08-05 11:48:26 +0200

    implement service auth with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ORA-Services';
+  constructor() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyDuGccWApor4wA510mYvWNZ1KK8Drl-5sI",
+      authDomain: "ora-services.firebaseapp.com",
+      databaseURL: "https://ora-services.firebaseio.com",
+      projectId: "ora-services",
+      storageBucket: "",
+      messagingSenderId: "688821252505",
+      appId: "1:688821252505:web:9df6a41048284545"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
